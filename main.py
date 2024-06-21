e4e81d84aa8c82781eb4e26f2a422caa3331882e
Beguwula <171718612+Beguwula@users.noreply.github.com>
2024-06-21 19:18:49 +0300

    Add InviteBlocker module

--- main.py
+++ main.py
@@ -34,4 +34,22 @@ async def on_member_join(member):
         except:
             print("User probably has dms off...")
 
+@bot.event
+async def on_message(message):
+    if module_list["InviteBlocker"]== True:
+        # Check if the message is in an allowed channel
+        allowed_channels = config.get("INVITEBLOCKER_ALLOWED_CHANNELS")
+        allowed_roles = config.get("INVITEBLOCKER_ALLOWED_ROLES")
+        whitelisted = any(role.id in allowed_roles for role in message.author.roles)
+
+        if message.channel.id not in allowed_channels and not whitelisted:
+            
+            # Convert message content to lowercase for case-insensitive check
+            content = message.content.lower()
+
+            # Check if the message contains an invite link
+            if "discord.gg" in content or "discordapp.com/invite" in content or "gg/" in content:
+                await message.delete()
+                await message.channel.send("Invite links are not allowed!")
+
 bot.run(key)
